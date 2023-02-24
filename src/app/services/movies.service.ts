45164e8d853c21397a3e5235c3132c41f3dcfc79
taranovska <93533926+taranovska@users.noreply.github.com>
2023-02-24 18:04:37 +0200

    cleaned-up

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../environments/environment';
+import { environment } from './../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
